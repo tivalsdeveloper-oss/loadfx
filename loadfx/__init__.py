35afb7c22f5f3ee383ab1d7b3b2219f61830db39
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-11 11:40:13 +0200

    Add loadfx public API

---
+++ loadfx/__init__.py
@@ -0,0 +1,6 @@
+from .loader import Loader, Spinner, ProgressBar
+from .text import TextFX
+from .menu import Menu
+
+__version__ = "1.0.0"
+__all__ = ["Loader", "Spinner", "ProgressBar", "TextFX", "Menu"]
